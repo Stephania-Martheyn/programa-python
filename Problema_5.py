8a13a11e3caa3b427b1e8b11249432ba0255931a
Stephania-Martheyn <stephaniemartheyn@hotmail.com>
2026-05-26 11:41:57 -0500

    Se quitan tildes

--- Problema_5.py
+++ Problema_5.py
@@ -2,7 +2,7 @@ def calcular_resumen_jornada(recurso, umbral=40):
 	nombre = recurso[0]
 	horas_semanales = recurso[1:]
 	total_horas = sum(horas_semanales)
-	clasificacion = "Sobretiempo" if total_horas > umbral else "Horario Estándar o inferior"
+	clasificacion = "Sobretiempo" if total_horas > umbral else "Horario Estandar o inferior"
 	return nombre, total_horas, clasificacion
 
 
@@ -18,5 +18,5 @@ for recurso in matriz_horas:
 	nombre, total_horas, clasificacion = calcular_resumen_jornada(recurso)
 	print(f"Recurso: {nombre}")
 	print(f"Total de horas semanales: {total_horas}")
-	print(f"Clasificación de jornada: {clasificacion}")
+	print(f"Clasificacion de jornada: {clasificacion}")
 	print("-" * 40)
